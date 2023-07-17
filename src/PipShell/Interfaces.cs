00195bcca50779c1d934faa8e18ed5939e9069a9
Floris Heyvaert <floris.heyvaert@gmail.com>
2023-07-17 11:52:35 +0200

    .net 7 bump

---
+++ src/PipShell/Interfaces.cs
@@ -0,0 +1,19 @@
+﻿using PipShell.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PipShell
+{
+    public interface IPip
+    {
+        Task<List<Package>> Get(CancellationToken cancellationToken = default);
+        Task<Package> Get(string name, CancellationToken cancellationToken = default);
+        Task<Package> Update(string name, CancellationToken cancellationToken = default);
+        Task<Package> Uninstall(string name, CancellationToken cancellationToken = default);
+        Task<Package> Install(string name, CancellationToken cancellationToken = default);
+    }
+}
--- src/PipShell/Python/Pip.cs
+++ src/PipShell/Python/Pip.cs
@@ -9,15 +9,6 @@ using System.Threading.Tasks;
 
 namespace PipShell.Python
 {
-    public interface IPip
-    {
-        Task<List<Package>> Get(CancellationToken cancellationToken = default);
-        Task<Package> Get(string name, CancellationToken cancellationToken = default);
-        Task<Package> Update(string name, CancellationToken cancellationToken = default);
-        Task<Package> Uninstall(string name, CancellationToken cancellationToken = default);
-        Task<Package> Install(string name, CancellationToken cancellationToken = default);
-    }
-
     public class Pip : IPip
     {
         private readonly IPipCommander _pipCommander;
